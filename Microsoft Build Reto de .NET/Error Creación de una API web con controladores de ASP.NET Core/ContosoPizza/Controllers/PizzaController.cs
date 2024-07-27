60b1c5d8f04a18223294a4f7d4443c3251a5cd54
Camilo Henao <camivallejo2010@gmail.com>
2024-07-26 23:44:33 -0500

    Inicio

---
+++ Microsoft Build Reto de .NET/Error Creación de una API web con controladores de ASP.NET Core/ContosoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,41 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
+
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
